e00d6514a75ebea66a540d4195b7936a38f61cf0
pallaviparteti <118168645+pallaviparteti@users.noreply.github.com>
2023-02-11 22:59:23 +0530

    Added EmployeePresentOrAbsent file

---
+++ EmployeePresentOrAbsent.java
@@ -0,0 +1,23 @@
+package com.employeewageAdressBookManagementSystem;
+
+public class EmployeePresentOrAbsent {
+	int IS_FULL_TIME =1;
+
+	public void toCheckEmployeePresentOrAbsent() {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is not present");
+		}
+	}
+	public static void main ( String args[]) {
+		EmployeePresentOrAbsent employeePresentOrAbsent = new EmployeePresentOrAbsent();
+		employeePresentOrAbsent.toCheckEmployeePresentOrAbsent();
+
+	}
+}
+
+
+
